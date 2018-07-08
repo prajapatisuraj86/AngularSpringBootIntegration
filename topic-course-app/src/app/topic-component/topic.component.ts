e8283c81db916a2b3f7e61fa656fa749e9f3bc5a
Suraj Prajapati <prajapatisuraj86@gmail.com>
2018-07-08 16:35:24 +0530

    LYZ

--- topic-course-app/src/app/topic-component/topic.component.ts
+++ topic-course-app/src/app/topic-component/topic.component.ts
@@ -13,16 +13,18 @@ import { Observable } from 'rxjs/Observable';
 export class TopicComponent {
 
     topics: Topic[];
-    responseStatus : String;
-    customObject : CustomObject;
-    deleteSuccess : boolean;
-    isView : boolean = true;
-    isUpdate : boolean = false;
-    isAdd : boolean = false;
-    topicId : String;
-    updateTopicObj : Topic;
-    public addTopicObj : Topic = new Topic();
-    public messageAlert : MessageDemoComponent = new MessageDemoComponent();
+    responseStatus: String;
+    customObject: CustomObject;
+    deleteSuccess: boolean;
+    isView: boolean = true;
+    isUpdate: boolean = false;
+    isAdd: boolean = false;
+    topicId: String;
+    isAddValidation : boolean = false;
+    errorMessage : String = '';
+    updateTopicObj: Topic;
+    public addTopicObj: Topic = new Topic();
+    public messageAlert: MessageDemoComponent = new MessageDemoComponent();
 
 
     constructor(private topicService: TopicService) { }
@@ -35,11 +37,11 @@ export class TopicComponent {
         this.topicService.getTopics().subscribe(customObject => this.topics = customObject.resObject);
     }
 
-    deleteTopic(topicId) : void {
+    deleteTopic(topicId): void {
         this.topicService.deleteTopic(topicId).subscribe(
             customObject => {
                 this.responseStatus = customObject.responseStatus;
-                if(this.isEqual(this.responseStatus,"SUCCESS")) {
+                if (this.isEqual(this.responseStatus, "SUCCESS")) {
                     this.deleteSuccess = true;
                     console.log("Deleted Successfully");
                     this.messageAlert.success("Deleted Sucessfully.");
@@ -53,7 +55,7 @@ export class TopicComponent {
         );
     }
 
-    enableUpdateTopic(topic) : void {
+    enableUpdateTopic(topic): void {
         this.isView = false;
         this.isUpdate = true;
         this.isAdd = false;
@@ -61,11 +63,11 @@ export class TopicComponent {
         this.topicId = this.updateTopicObj.id;
     }
 
-    updateTopic(topic) : void {
+    updateTopic(topic): void {
         this.topicService.updateTopic(topic, topic.id).subscribe(
             customObject => {
                 this.responseStatus = customObject.responseStatus;
-                if(this.isEqual(this.responseStatus,"SUCCESS")) {
+                if (this.isEqual(this.responseStatus, "SUCCESS")) {
                     console.log("Topic Updated Successfully");
                 } else {
                     console.log("Error occoured while updating Topics");
@@ -83,32 +85,45 @@ export class TopicComponent {
         this.isUpdate = false;
     }
 
-    addTopic() : void {
-        this.topicService.addTopic(this.addTopicObj).subscribe(
-            customObject => {
-                this.responseStatus = customObject.responseStatus;
-                if(this.isEqual(this.responseStatus,"SUCCESS")) {
-                    console.log("Topic added Successfully");
-                } else {
-                    console.log("Error occoured while updating Topics");
+    addTopic(): void {
+        if (this.addTopicObj.id == null || this.addTopicObj.id.trim().length <= 0) {
+            this.isAddValidation = true;
+            this.errorMessage = "Topic Id is Mandatory";
+        } else if (this.addTopicObj.name == null || this.addTopicObj.name.trim().length <= 0) {
+            this.isAddValidation = true;
+            this.errorMessage = "Topic Name is Mandatory";
+        } else if (this.addTopicObj.description == null || this.addTopicObj.description.trim().length <= 0) {
+            this.isAddValidation = true;
+            this.errorMessage = "Topic Description is Mandatory";
+        } else {
+            this.topicService.addTopic(this.addTopicObj).subscribe(
+                customObject => {
+                    this.responseStatus = customObject.responseStatus;
+                    if (this.isEqual(this.responseStatus, "SUCCESS")) {
+                        console.log("Topic added Successfully");
+                    } else {
+                        console.log("Error occoured while updating Topics");
+                    }
+                    this.isAdd = false;
+                    this.isAddValidation = false;
+                    this.getTopics();
+                    return true;
                 }
-                this.isAdd = false;
-                this.getTopics();
-                return true;
-            }
-        );
+            );
+        }
     }
 
-    cancelAdd() : void {
+    cancelAdd(): void {
         this.isAdd = false;
+        this.isAddValidation = false;
     }
 
-    enableAddTopic() : void {
+    enableAddTopic(): void {
         this.addTopicObj = new Topic();
         this.isAdd = true;
     }
 
-    isEqual(var1 : String, var2 : String) : boolean {
+    isEqual(var1: String, var2: String): boolean {
         return var1 == var2;
     }
 
